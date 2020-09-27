c040c169524c52b90adde5a1b105ee07d697dc5e
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2020-09-27 12:29:50 -0400

    docs: add file headers

--- util.py
+++ util.py
@@ -1,3 +1,7 @@
+### Codebench Dataset Extractor by Marcos Lima (marcos.lima@icomp.ufam.edu.br)
+### Universidade Federal do Amazonas - UFAM
+### Instituto de Computação - IComp
+
 import logging
 import os
 from collections import Counter
